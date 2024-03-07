0577e04cccabdf691c3477568e81d7203e43fd57
Hamza Saktiwy <hsaktiwy@e1r7p5.1337.ma>
2024-03-07 22:01:56 +0100

    i added redis service succesfully

--- srcs/requirements/redis/tools/script.sh
+++ srcs/requirements/redis/tools/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # modifie the bind 127.0.0.1 to be  0.0.0.0
-sed -i "s/127.0.0.1/0.0.0.0/g" /etc/redis/redis.conf
+sed -i "s/127.0.0.1 ::1/0.0.0.0/g" /etc/redis/redis.conf
+# echo -i "s/# requirepass foobared/requirepass $REDIS_PASS/g" /etc/redis/redis.conf
 # service start 
-service redis-server start
+redis-server --requirepass $REDIS_PASS
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -17,18 +17,20 @@
     wp --allow-root core download
 # create our wp-config file
     wp config create  --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
-# we will modife the wp-confige to sweet our redis we need to identife the redis host port and connection type tcp in this case
-    wp --allow-root config set WP_REDIS_HOST 'localhost' --type=constant
-    wp --allow-root config set WP_REDIS_PORT 6379 --type=constant
-    wp --allow-root config set WP_REDIS_SCHEME 'tcp' --type=constant
-# install redis cache plugin
-    wp --allow-root plugin install redis-cache --activate
-# this is for activating our redis cache object from the script and not manulay
-    #wp --allow-root plugin activate redis-cache
 # create a our user admin (OUR FIRST USER EVER)
     wp --allow-root core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 # create a our second user
     wp --allow-root user create hamza hsaktiwy-sak@hotmail.com --role=$WP_USER_ROLE --user_pass=$WP_USER_PASSWORD
+# we will modife the wp-confige to sweet our redis we need to identife the redis host port and connection type tcp in this case
+    wp --allow-root config set WP_REDIS_HOST 'redis' --type=constant
+    wp --allow-root config set WP_REDIS_PORT 6379 --type=constant
+    wp --allow-root config set WP_REDIS_PASSWORD $REDIS_PASS --type=constant
+# install redis cache plugin
+    wp --allow-root plugin install redis-cache --activate
+#this this will allow our redis to work on our wordpress
+    chown -R www-data:www-data /var/www/wordpress
+#this is for activating our redis cache object from the script and not manulay
+    wp --allow-root redis enable
 # configurate our www.conf
     # change the listen value to be able to receive it requests from wordpress one port 9000  
         sed -i "s#listen = /run/php/php7.4-fpm.sock#listen=wordpress:9000#" /etc/php/7.4/fpm/pool.d/www.conf
