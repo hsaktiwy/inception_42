a43ecb5f92467ec563981cdfa70d46455155ccef
hsaktiwy <hsaktiwy@gmail.com>
2024-09-02 17:17:29 +0100

    FTp ended adminer in process

--- srcs/requirements/adminer/tools/script.sh
+++ srcs/requirements/adminer/tools/script.sh
@@ -1,3 +1,11 @@
+# create the adminer index.php folder
+mkdir /var/www/html/adminer
+# install adminer
+cd  /var/www/html/adminer
+wget -O index.php https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php
+# gave the apache permission on our folder
+chmod 744 /var/www/html/adminer
+# enable adminer site
 sudo a2ensite adminer.conf
-sudo a2enmod proxy_fcgi
-apache -D FOREGROUND
+
+# apache -D FOREGROUND
--- srcs/requirements/ftp/tools/script.sh
+++ srcs/requirements/ftp/tools/script.sh
@@ -11,6 +11,14 @@ mkdir -p $FTP_ROOT
         mkdir -p /var/run/vsftpd/empty
 # let add our user to our ftp userlist file :vsftpd.userlist (tee -a : will added and append the echo value to our userlist file)
     echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
+
+    # added the user of ftp to the ww-dat groupd allowing him to have the privilege to edit/create a file in the hosted folder
+    usermod -aG www-data $FTP_USER
+    # i notice that the www-data gourp permission on the hosted file are only read and execute so let add write
+    chmod 775 $FTP_ROOT
+    # and like this we allowed our ftp user to access ftp hosted folder properly (i think hahaha! or maybe not?)
+
+# added the local_root=$FTP_ROOT to the vsftpd.conf at the end of the configuration
 echo "local_root=$FTP_ROOT" >> /etc/vsftpd.conf
 # lets run our ftp server
 exec vsftpd /etc/vsftpd.conf
