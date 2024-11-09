4e9f52ace28f8f5cb0eab7774568b485f1a7f922
hsaktiwy <hsaktiwy@e1r4p9.1337.ma>
2024-11-09 17:54:40 +0100

    nginx update

--- srcs/requirements/bonus/redis/tools/script.sh
+++ srcs/requirements/bonus/redis/tools/script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # modifie the bind 127.0.0.1 to be  0.0.0.0
 sed -i "s/127.0.0.1 ::1/0.0.0.0/g" /etc/redis/redis.conf
-# echo -i "s/# requirepass foobared/requirepass $REDIS_PASS/g" /etc/redis/redis.conf
 # service start 
 redis-server --requirepass $REDIS_PASS
