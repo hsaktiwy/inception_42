fb3a16585d3015037a9aaa703a90930059255c5d
hsaktiwy <hsaktiwy@gmail.com>
2024-11-11 17:40:40 +0100

    fixed some bugs in the configuration of the nginx

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -3,7 +3,7 @@ mkdir -p $CRT_PATH
 sed -i "s#PROJECT.CRT#$INCEP_CRT#g" /etc/nginx/nginx.conf
 sed -i "s#PROJECT.KEY#$INCEP_KEY#g" /etc/nginx/nginx.conf
 sed -i "s#NGX_ROOT_PATH#$NGX_ROOT_PATH#g" /etc/nginx/nginx.conf
-sed -i "s#SERVER_NAME#$URL_WP#g" /etc/nginx/nginx.conf
+sed -i "s#SERVER_NAME#$WP_URL#g" /etc/nginx/nginx.conf
 sed -i "s#CGI_PASS#$FASTCGI_PASS#g" /etc/nginx/nginx.conf
 openssl req -x509 -nodes -out $INCEP_CRT -keyout $INCEP_KEY -subj $SSL_INFO
 nginx -g "daemon off;"
